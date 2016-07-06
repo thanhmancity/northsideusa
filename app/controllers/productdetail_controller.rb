15cadcf84ff36c470dbf8fc51f63264ef9a9c99c
Jeff Johnson <uhohchongo@hotmail.com>
2016-07-06 08:06:41 -0700

    Adds colors to mobile navigation; improves details box on PDP; adjusts size query for PDP

--- app/controllers/productdetail_controller.rb
+++ app/controllers/productdetail_controller.rb
@@ -4,7 +4,7 @@ class ProductdetailController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @sizes = Size.where(product_id: @product.id)
+    @sizes = Size.where(product_color_id: @product.id)
     @order_item = current_order.order_items.new
   end
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,3 @@
 class Size < ActiveRecord::Base
-  belongs_to :product
+  belongs_to :product_color
 end
