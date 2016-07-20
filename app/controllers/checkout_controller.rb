76ff971f721a3c10af81e369107011fa73a2e4db
Jeff Johnson <uhohchongo@hotmail.com>
2016-07-20 11:39:33 -0700

    Revert "Re-configures shipping/billing object creation"
    This reverts commit 7759cb545e1b447a566dac3d126ab577cbcc48b2.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,9 @@
 class CheckoutController < ApplicationController
   def show
     @order = current_order
+    @order_shipping = OrderShipping.create(:order_id => @order.id)
+    @order_billing = OrderBilling.create(:order_id => @order.id)
+    @order.save
   end
   def shipping
   end
