e57f45d52c0d659b5ac6a539e32d59d777e1afbb
Uh O Chongo <uhohchongo@hotmail.com>
2016-08-11 22:47:23 +0000

    Remove non-working smtp goo

--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "server.triplettrading.com",
-  :port                 => 25,
-  :domain               => "northsideusa.com",
-  :user_name            => "jeffj",
-  :password             => "1TripleT!",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
