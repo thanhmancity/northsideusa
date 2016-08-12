a14ee2c5460b725149a263976e98175e9c2237ff
Uh O Chongo <uhohchongo@hotmail.com>
2016-08-12 18:45:52 +0000

    Revert smtp goo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,15 +16,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address        => 'server.triplettrading.com',
-      :port           => '25',
-      :authentication => :login,
-      :user_name      => 'jeffj@triplettrading.com',
-      :password       => '1TripleT!',
-      :domain         => 'northsideusa.com',
-      :enable_starttls_auto => true
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
