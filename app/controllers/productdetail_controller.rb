fdab767700010d24cbb1bc42c7c75e45af528625
Uh O Chongo <uhohchongo@hotmail.com>
2016-09-29 22:37:07 +0000

    Workaround for missing sizes on PDP size dropdown

--- app/controllers/productdetail_controller.rb
+++ app/controllers/productdetail_controller.rb
@@ -5,7 +5,7 @@ class ProductdetailController < ApplicationController
   def show
     @product_color = ProductColor.find(params[:id])
     @product = Product.where(id: @product_color.product_id)
-    @sizes = Size.where(product_color_id: params[:id])
+    @sizes = Size.where(product_color_id: params[:id]).where('has_stock != 0')
     @order_item = current_order.order_items.new
   end
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,11 @@
 class Size < ActiveRecord::Base
-  belongs_to :product_color
+    belongs_to :product_color
+  
+    def disable_select 
+        if self.has_stock == 0
+            true
+        else 
+            false
+        end
+    end
 end
