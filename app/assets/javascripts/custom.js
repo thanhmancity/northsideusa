35751ea36eb21338679a0c8940cb9fa750e30194
Thanh Manci <mahoangtienthanh@gmail.com>
2017-11-22 15:11:09 +0700

    Done Fix Filter and Sorter

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,15 +2,28 @@ $(document).on('ready', function () {
   $(document).on('click','.rootTreeCategory', function(){
     var dataList = $(this).attr('data-list');
     var status = $(this).attr('data-active');
+    $('.categoryPlusBtn i').attr('class', 'fa fa-plus');
+    $('.categoryFilterBox .categoryItem span').attr('data-active', 'active');
     if(status === 'active'){
       $(this).attr('data-active', 'unActive');
-      $(this).children('span').css('background-color', '#686868');
+      $(this).children().children().attr('class', 'fa fa-minus');
+      $('.navListCategory').fadeOut();
       $('#' + dataList).fadeIn('3000');
     }else{
       $(this).attr('data-active', 'active');
-      $(this).children('span').css('background-color', '#393B38');
+      $(this).children().children().attr('class', 'fa fa-plus');
       $('#' + dataList).fadeOut('3000');
     }
+  });
+
+  $(document).on('click', '.radioBtnFilter', function(){
+    if($(this).attr('checked') === 'checked'){
+      $(this).next().trigger('click');
+    }
+  });
+
+  $(document).on('click', '.filterLink', function(){
+    $(this).prev().attr('checked', 'checked');
   });
 
   var sizesSlider = $('#sizesSlider').freshslider({
