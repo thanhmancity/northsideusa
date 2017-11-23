9671894f353874e8a3a6a793a46694b186c90f97
Thanh Manci <mahoangtienthanh@gmail.com>
2017-11-23 08:50:32 +0700

    Done Fix Filter and Sorter

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -25,48 +25,4 @@ $(document).on('ready', function () {
   $(document).on('click', '.filterLink', function(){
     $(this).prev().attr('checked', 'checked');
   });
-
-  var sizesSlider = $('#sizesSlider').freshslider({
-      step: 0.5,
-      range: false,
-      min: parseFloat($('#sizesSlider').attr('data-min-size')),
-      max: parseFloat($('#sizesSlider').attr('data-max-size')),
-      value: parseFloat($('#sizesSlider').attr('data-min-size'))
-  });
-
-  var priceSlider = $('#priceSlider').freshslider({
-    range: true,
-    unit:'$',
-    step: 0.1,
-    min: Math.floor(parseFloat($('#priceSlider').attr('data-min-size'))),
-    max: parseFloat($('#priceSlider').attr('data-max-size')),
-    value: [Math.floor(parseFloat($('#priceSlider').attr('data-min-size'))), Math.floor(parseFloat($('#priceSlider').attr('data-min-size')))],
-    onchange:function(low, high){
-      $('#priceSlider input').val([low + '..' + high]);
-    }
-  });
-
-
-  $('#priceSlider').on('mouseup','.fscaret', function(){
-    var url = '/men/trail';
-    var min_price = parseFloat($('#priceSlider .fscaret').prev().text());
-    var max_price = parseFloat($('#priceSlider .fscaret').next().text());
-    $.ajax({
-    url: url,
-      dataType: 'jsonp',
-      type: 'GET',
-      data: {min_price: min_price, max_price: max_price, filter_type: 'price'}
-    });
-  });
-
-  $('#sizesSlider').on('mouseup','.fscaret', function(){
-    var url = '/men/trail';
-    var size = parseFloat($('#sizesSlider .fscaret').text());
-    $.ajax({
-    url: url,
-      dataType: 'jsonp',
-      type: 'GET',
-      data: {size: size, filter_type: 'size'}
-    });
-  });
 });
